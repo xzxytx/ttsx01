52256e567c27babc47d248c144a7d0bcb51133d7
yu <392296204@qq.com>
2017-07-04 23:51:52 +0800

    添加ｓｔａｔｉｃ

--- fresh/tt_user/models.py
+++ fresh/tt_user/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
+    uemail = models.CharField(max_length=40)
